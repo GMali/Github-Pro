a58d2e0343febb4e733565e4596b3419f16c4c98
Gaurav Mali <hello@gauravmali.com>
2014-11-04 21:59:29 -0800

    Features are fun in background tabs as well
    Fixes #4

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,19 +4,21 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('Github Pro\nVersion: ', details.previousVersion);
 });
 
+
+// Every time a tab has been updated (first load or dom change via ajax)
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
+    // And has completed loading
     if (changeInfo.status === 'complete') {
-        var url = new URL(tab.url);
 
+        // And if the tab is a Github tab
+        var url = new URL(tab.url);
         if ( url.origin === 'https://github.com') {
 
-            // The current open tab will be listening to this message if it is
-            // running contentscript.js. Which, only runs on github.com
-            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {msg: 'msg'}, function(msg) {
-                    console.log(msg);
-                });
+            // Send a message to that page's contentscript!
+            // Basically, the message listener will run all the features
+            chrome.tabs.sendMessage(tab.id, {msg: 'page has been loaded'}, function(msg) {
+                console.log(msg);
             });
         }
     }
