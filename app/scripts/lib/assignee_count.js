2148ac332004b6c18323aa8947a5562ef847dc23
Gaurav Mali <hello@gauravmali.com>
2014-11-04 20:00:06 -0800

    Assginee count work as assignee filters now
    Resolves #9

--- app/scripts/lib/assignee_count.js
+++ app/scripts/lib/assignee_count.js
@@ -11,7 +11,7 @@
 function assigneeCount() {
     var assignees         = {},
         sortedAssignees   = [],
-        $avatars          = $('.table-list-cell-avatar a img'),
+        $avatars          = $('.table-list-cell-avatar a'),
         // Features might have this pattern, so writing it this way to make it
         // easier to refactor later
         containerClass    = 'github-pro-assignee-count',
@@ -26,16 +26,20 @@ function assigneeCount() {
 
         // Collect assignee data from DOM
         $avatars.each(function() {
-            var user = $(this).attr('alt');
-            assignees[user] = (assignees[user] || 0) + 1;
+            var username     = $(this).attr('aria-label').replace('View everything assigned to ', ''),
+                userFullName = $(this).find('img').attr('alt');
+
+            // Store it like hashmap
+            assignees[username] = _incrementAssignee(username, userFullName, assignees[username]);
         });
 
         // Sort
-        sortedAssignees = sortAssignees(assignees);
+        sortedAssignees = _sortAssignees(assignees);
 
         // Create the container
         sortedAssignees.forEach(function (assignee) {
-            $container.prepend('<span class="subnav-item">' + assignee[0] + ': ' + assignee[1] + '</span>');
+            var $badge = _createBadge(assignee);
+            $container.prepend($badge);
         });
 
         // Render!
@@ -43,13 +47,87 @@ function assigneeCount() {
     }
 }
 
+
+/*******************************************************************************
+ * HELPER FUNCTIONS
+ *******************************************************************************
+
+
+/**
+ * Keep track of assginee duplicates. Classic hashmap solution for duplicates,
+ * but the value has extra information about the assignee/user
+ * @param  {String} username     assignee's Github username (unique)
+ * @param  {String} userFullName assignee's full name (Firstname Lastname)
+ * @param  {Object} assignee     value of the hashmap.
+ * @return {Object}              updated value of the hashmap
+ */
+function _incrementAssignee (username, userFullName, assignee) {
+    if (assignee) {
+        assignee.count++;
+    } else {
+        assignee = {
+            count    : 1,
+            fullName : userFullName,
+            username : username
+        };
+    }
+
+    return assignee;
+}
+
 /**
  * Sorts an object by it's value
  * @param  {Object} assignees key: String, value: Number
  * @return {Array}            sorted object values. [ ["name", count], ... ]
  */
-function sortAssignees(assignees) {
+function _sortAssignees (assignees) {
     return Object.keys(assignees)
-            .map(function (user) { return [user, assignees[user]]; })
-            .sort(function (user1, user2) { return user1[1] - user2[1]; });
+            .map(function (username) { return [username, assignees[username]]; })
+            .sort(function (user1, user2) { return user1[1].count - user2[1].count; });
+}
+
+/**
+ * Creates an badge-like element that has the assignee name and count. it is
+ * rendered as an anchor tag with href that will toggle the assignee filter
+ * @param  {String}         assignee an assignee object from the assignees hash
+ * @return {jQuery Object}           the object that needs to get rendered
+ */
+function _createBadge (assignee) {
+    var href = _createHref(assignee[1].username);
+
+    var $badge = $('<a>')
+        .addClass('subnav-item')
+        .attr('href', href)
+        .attr('data-github-pro-username', assignee[0])
+        .html(assignee[1].fullName + ': ' + assignee[1].count);
+
+    return $badge;
+}
+
+/**
+ * Creates a href for a user based on the queries in the current URL
+ * This will allow user to toggle the assignee filters for PRs
+ * @param  {String} username the unique Github username of a user
+ * @return {String}          the final URL in string format. Usable as a href
+ */
+function _createHref (username) {
+    var prLink             = new URL(window.location.href),
+        userQuery          = '+assignee%3A' + username,
+        defaultQuery       = '?q=is%3Apr+is%3Aopen',
+        redirectedPathname = '/assigned/';
+
+    if (prLink.search === '') {
+        // Github somtimes redirects to ../pulls/assigned/[username]
+        var queryPosition = prLink.pathname.indexOf(redirectedPathname);
+        prLink.pathname = prLink.pathname.slice(0, queryPosition);
+        prLink.search = defaultQuery;
+
+    } else if (prLink.search.indexOf(userQuery) === -1) {
+        prLink.search += userQuery;
+
+    } else {
+        prLink.search = prLink.search.replace(userQuery, '');
+    }
+
+    return prLink.toString();
 }
