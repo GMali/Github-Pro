462fa4336ccd0a885903a2895b7b288099505dde
Gaurav Mali <hello@gauravmali.com>
2016-06-13 20:20:22 -0700

    Toggle all notes button only rendered once

--- app/scripts/lib/toggle_notes.js
+++ app/scripts/lib/toggle_notes.js
@@ -3,16 +3,21 @@
 /*jshint unused:false */
 
 function toggleNotes() {
-    var siblingSelector = '.diffbar .diffbar-item.js-select-menu',
-        buttonSelector = '.js-toggle-all-notes',
-        $singleToggles = $('.show-file-notes input'),
-        buttonTemplate  = '<div class="diffbar-item"><label><input type="checkbox" checked="checked" class="js-toggle-all-notes" style="margin-right: 5px;">Show all notes</label></div>';
+    var toggleButtonSelector = '.js-toggle-all-notes';
+
+    // Don't run this if we've already added the button
+    if ($(toggleButtonSelector).length) {
+        return;
+    }
+
+    var $singleToggles = $('.show-file-notes input'),
+        buttonTemplate = '<div class="diffbar-item"><label><input type="checkbox" checked="checked" class="js-toggle-all-notes" style="margin-right: 5px;">Show all notes</label></div>';
 
     // Add the checkbox
-    $(siblingSelector).after(buttonTemplate);
+    $('.diffbar .diffbar-item.js-select-menu').after(buttonTemplate);
 
     // Easier to simulate the click than toggling all comments and checkboxes
-    $(buttonSelector).change(function(event) {
+    $(toggleButtonSelector).change(function(event) {
         $.each($singleToggles, function(index, value) {
             value.click();
         });
